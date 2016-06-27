c67e7ec366a3be805a094ae73ec16a125d4233de
luckerdj <dujing.chart@gmail.com>
2016-06-27 14:16:22 +0800

    add cert

--- BatchTrans.php
+++ BatchTrans.php
@@ -14,11 +14,33 @@ use minms\alipay\Config;
 
 class BatchTrans
 {
-    public function __construct($config = [])
+    private $alipay_submit;
+
+    public function __construct()
     {
-        if(empty($config) && empty(Config::$partner)){
+        if(empty(Config::$partner)){
             throw new \Exception('请设置商户ID');
         }
+
+        Config::$cacert = realpath('batch-trans/cert/cacert.pem');
+
+        $this->alipay_submit = new \AlipaySubmit([
+            'partner' => Config::$partner,
+            'key' => Config::$key,
+            'sign_type' => Config::$sign_type,
+            'input_charset' => Config::$input_charset,
+            'cacert' => Config::$cacert,
+            'transport' => Config::$transport
+        ]);
+    }
+
+
+    public function buildRequestHttp($params){
+        return $this->alipay_submit->buildRequestHttp($params);
+    }
+
+    public function buildRequestForm($params){
+        return $this->alipay_submit->buildRequestForm($params, 'post', 'submit');
     }
 
 
