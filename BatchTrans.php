8279764a32b0fa498bcc77a40ec001e0dc1cc8d7
luckerdj <dujing.chart@gmail.com>
2016-06-27 11:41:18 +0800

    add psr-4

--- BatchTrans.php
+++ BatchTrans.php
@@ -8,8 +8,18 @@
 
 namespace minms\alipay;
 
+require 'batch-trans/src/alipay_submit.class.php';
+
+use minms\alipay\Config;
 
 class BatchTrans
 {
+    public function __construct($config = [])
+    {
+        if(empty($config) && empty(Config::$partner)){
+            throw new \Exception('请设置商户ID');
+        }
+    }
+
 
 }
